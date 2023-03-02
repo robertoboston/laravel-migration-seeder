beb2a4debd42f990c52be3a6a3a855442ffa5c63
roberto.boston <roberto.bozzo97@tiscali.it>
2023-03-02 15:58:47 +0100

    migrate done

--- database/migrations/2023_03_02_144840_create_trains_table.php
+++ database/migrations/2023_03_02_144840_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda',50);
+            $table->string('stazione_di_partenza',100);
+            $table->string('stazione_di_arrivo',100);
+            $table->time('orario_di_partenza');
+            $table->time('orario_di_arrivo');
+            $table->string('codice_treno',50);
+            $table->string('numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('cancellato');
             $table->timestamps();
         });
     }
